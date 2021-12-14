d3313a1d0d9e9d7c7ad3974b662677f2b4cb7fa7
Jack Pena <penajackc@gmail.com>
2021-12-14 09:39:04 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arr, event){
+    const namesArr = [];
+
+    for(let i = 0; i < arr.length; i++){
+        namesArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return namesArr;
+}
+
+function countDown(posNum){
+    while(posNum >= 0){
+        console.log(posNum);
+        posNum--;
+    }
+}
